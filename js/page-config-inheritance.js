d8c54d9d045a487413306cde667c4a3cc47330c4
Christoph Stickel <christoph@neochic.de>
2016-05-21 03:18:07 +0200

    * fixing jshint

--- js/page-config-inheritance.js
+++ js/page-config-inheritance.js
@@ -1,3 +1,5 @@
+/* globals document */
+
 define(['jquery'], function($) {
     $(document).on('change keyup input propertychange', '[id^=woodlets_page_setting_]:not(.inherit-input)', function() {
         $('#' + $(this).attr('id') + '_inherit').prop('checked', false);
