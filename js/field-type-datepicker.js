8f53b417c0df8fe4d2491e7a283fbc93d53c7161
Christoph Stickel <christoph@neochic.de>
2017-04-13 20:31:11 +0200

    * fixed german datepicker datetime format

--- js/field-type-datepicker.js
+++ js/field-type-datepicker.js
@@ -21,90 +21,145 @@ define([
 
     Inputmask.extendAliases({
         custom01: {
-            mask: "d.m.y - h:s",
-            placeholder: "dd.mm.yyyy - hh:mm",
+            mask: "1.2.y - h:s",
+            placeholder: "TT.MM.JJJJ - SS:MM",
+            alias: "dd/mm/yyyy",
             regex: {
-                hrspre: new RegExp("[012]"),
+                hrspre: new RegExp("[012]"), //hours pre
                 hrs24: new RegExp("2[0-4]|1[3-9]"),
-                hrs: new RegExp("[01][0-9]|2[0-4]"),
+                hrs: new RegExp("[01][0-9]|2[0-4]"), //hours
                 ampm: new RegExp("^[a|p|A|P][m|M]"),
-                mspre: new RegExp("[0-5]"),
+                mspre: new RegExp("[0-5]"), //minutes/seconds pre
                 ms: new RegExp("[0-5][0-9]")
             },
+            separator: ".",
             timeseparator: ":",
-            hourFormat: "24",
+            hourFormat: "24", // or 12
             definitions: {
-                h: {
-                    validator: function(chrs, maskset, pos, strict, opts) {
-                        if ("24" === opts.hourFormat && 24 === parseInt(chrs, 10)) return maskset.buffer[pos - 1] = "0",
-                            maskset.buffer[pos] = "0", {
-                            refreshFromBuffer: {
-                                start: pos - 1,
-                                end: pos
-                            },
-                            c: "0"
-                        };
+                "h": { //hours
+                    validator: function (chrs, maskset, pos, strict, opts) {
+                        if (opts.hourFormat === "24") {
+                            if (parseInt(chrs, 10) === 24) {
+                                maskset.buffer[pos - 1] = "0";
+                                maskset.buffer[pos] = "0";
+                                return {
+                                    "refreshFromBuffer": {
+                                        start: pos - 1,
+                                        end: pos
+                                    },
+                                    "c": "0"
+                                };
+                            }
+                        }
+
                         var isValid = opts.regex.hrs.test(chrs);
-                        if (!strict && !isValid && (chrs.charAt(1) === opts.timeseparator || -1 !== "-.:".indexOf(chrs.charAt(1))) && (isValid = opts.regex.hrs.test("0" + chrs.charAt(0)))) return maskset.buffer[pos - 1] = "0",
-                            maskset.buffer[pos] = chrs.charAt(0), pos++, {
-                            refreshFromBuffer: {
-                                start: pos - 2,
-                                end: pos
-                            },
-                            pos: pos,
-                            c: opts.timeseparator
-                        };
-                        if (isValid && "24" !== opts.hourFormat && opts.regex.hrs24.test(chrs)) {
+                        if (!strict && !isValid) {
+                            if (chrs.charAt(1) === opts.timeseparator || "-.:".indexOf(chrs.charAt(1)) !== -1) {
+                                isValid = opts.regex.hrs.test("0" + chrs.charAt(0));
+                                if (isValid) {
+                                    maskset.buffer[pos - 1] = "0";
+                                    maskset.buffer[pos] = chrs.charAt(0);
+                                    pos++;
+                                    return {
+                                        "refreshFromBuffer": {
+                                            start: pos - 2,
+                                            end: pos
+                                        },
+                                        "pos": pos,
+                                        "c": opts.timeseparator
+                                    };
+                                }
+                            }
+                        }
+
+                        if (isValid && opts.hourFormat !== "24" && opts.regex.hrs24.test(chrs)) {
+
                             var tmp = parseInt(chrs, 10);
-                            return 24 === tmp ? (maskset.buffer[pos + 5] = "a", maskset.buffer[pos + 6] = "m") : (maskset.buffer[pos + 5] = "p",
-                                maskset.buffer[pos + 6] = "m"), tmp -= 12, 10 > tmp ? (maskset.buffer[pos] = tmp.toString(),
-                                maskset.buffer[pos - 1] = "0") : (maskset.buffer[pos] = tmp.toString().charAt(1),
-                                maskset.buffer[pos - 1] = tmp.toString().charAt(0)), {
-                                refreshFromBuffer: {
+
+                            if (tmp === 24) {
+                                maskset.buffer[pos + 5] = "a";
+                                maskset.buffer[pos + 6] = "m";
+                            } else {
+                                maskset.buffer[pos + 5] = "p";
+                                maskset.buffer[pos + 6] = "m";
+                            }
+
+                            tmp = tmp - 12;
+
+                            if (tmp < 10) {
+                                maskset.buffer[pos] = tmp.toString();
+                                maskset.buffer[pos - 1] = "0";
+                            } else {
+                                maskset.buffer[pos] = tmp.toString().charAt(1);
+                                maskset.buffer[pos - 1] = tmp.toString().charAt(0);
+                            }
+
+                            return {
+                                "refreshFromBuffer": {
                                     start: pos - 1,
                                     end: pos + 6
                                 },
-                                c: maskset.buffer[pos]
+                                "c": maskset.buffer[pos]
                             };
                         }
+
                         return isValid;
-                    },
+                    }
+                    ,
                     cardinality: 2,
-                    prevalidator: [ {
-                        validator: function(chrs, maskset, pos, strict, opts) {
+                    prevalidator: [{
+                        validator: function (chrs, maskset, pos, strict, opts) {
                             var isValid = opts.regex.hrspre.test(chrs);
-                            return strict || isValid || !(isValid = opts.regex.hrs.test("0" + chrs)) ? isValid : (maskset.buffer[pos] = "0",
-                                pos++, {
-                                pos: pos
-                            });
+                            if (!strict && !isValid) {
+                                isValid = opts.regex.hrs.test("0" + chrs);
+                                if (isValid) {
+                                    maskset.buffer[pos] = "0";
+                                    pos++;
+                                    return {
+                                        "pos": pos
+                                    };
+                                }
+                            }
+                            return isValid;
                         },
                         cardinality: 1
-                    } ]
-                },
-                s: {
+                    }]
+                }
+                ,
+                "s": { //seconds || minutes
                     validator: "[0-5][0-9]",
                     cardinality: 2,
-                    prevalidator: [ {
-                        validator: function(chrs, maskset, pos, strict, opts) {
+                    prevalidator: [{
+                        validator: function (chrs, maskset, pos, strict, opts) {
                             var isValid = opts.regex.mspre.test(chrs);
-                            return strict || isValid || !(isValid = opts.regex.ms.test("0" + chrs)) ? isValid : (maskset.buffer[pos] = "0",
-                                pos++, {
-                                pos: pos
-                            });
+                            if (!strict && !isValid) {
+                                isValid = opts.regex.ms.test("0" + chrs);
+                                if (isValid) {
+                                    maskset.buffer[pos] = "0";
+                                    pos++;
+                                    return {
+                                        "pos": pos
+                                    };
+                                }
+                            }
+                            return isValid;
                         },
                         cardinality: 1
-                    } ]
-                },
-                t: {
-                    validator: function(chrs, maskset, pos, strict, opts) {
+                    }]
+                }
+                ,
+                "t": { //am/pm
+                    validator: function (chrs, maskset, pos, strict, opts) {
                         return opts.regex.ampm.test(chrs + "m");
-                    },
+                    }
+                    ,
                     casing: "lower",
                     cardinality: 1
                 }
-            },
-            insertMode: !1,
-            autoUnmask: !1
+            }
+            ,
+            insertMode: false,
+            autoUnmask: false
         }
     });
 
